fe155db94007561679ae2faf1eba8f37bdd92998
DA <DA@DESKTOP-IMUTTN3>
2021-03-02 19:44:21 -0500

    2nd Part of Instagram Clone

---
+++ app/src/main/java/com/codepath/davida/parseinstagram/fragments/ProfileFragment.java
@@ -0,0 +1,51 @@
+package com.codepath.davida.parseinstagram.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.davida.parseinstagram.Post;
+import com.codepath.davida.parseinstagram.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileFragment extends PostsFragment {
+
+    @Override
+    protected void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: "+ post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+                swipeContainer.setRefreshing(false);
+            }
+        });
+    }
+}
